548dc3e66e1c5cfa0f7036bd89dc490668c4732c
Biel Frontera <biel.fb@gmail.com>
2017-01-17 02:38:17 +0100

    Add Inca data and assets

---
+++ src/data/inca/meta_consolidat_poldespesa.js
@@ -0,0 +1,26 @@
+window.OpenBudget = {"data":{"meta":{
+    "hierarchy": ["Política de despesa", "Capítol", "Partida"],
+    "page_title": "Pressupost de l'Ajuntament d'Inca",
+    "description": "",
+    "h1": "Ajuntament d'Inca",
+    "h2": "Projecte de pressupost",
+    "data_link": "",
+    "data_title": "Descarrega les dades",
+    "uservoice": "",
+    "base_headline": "PRESSUPOST CONSOLIDAT PER POLÍTICA DE DESPESA",
+    "gross_cost_label": "Despeses",
+    "revenue_label": "Ingressos",
+    "currency_sufix": " €",
+    "data_url": "data/inca/2017_consolidat_poldespesa.json",
+    // "cache_url": "data/inca/cache.json",    
+    "value": {
+        "label": "Pressupost 2017",
+        "type": "accounts",
+        "year": "2017"
+    },
+    "value2": {
+        "label": "Pressupost 2016",
+        "type": "accounts",
+        "year": "2016"
+    }
+}}};
