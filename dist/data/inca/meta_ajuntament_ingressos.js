9a5d8db59c9e489a58cdb51ef6f007c35228f609
Biel Frontera <biel.fb@gmail.com>
2017-01-17 02:43:16 +0100

    New version. Dist files

---
+++ dist/data/inca/meta_ajuntament_ingressos.js
@@ -0,0 +1,26 @@
+window.OpenBudget = {"data":{"meta":{
+    "hierarchy": ["Capítol", "Article", "Partida"],
+    "page_title": "Pressupost de l'Ajuntament d'Inca",
+    "description": "",
+    "h1": "Ajuntament d'Inca",
+    "h2": "Projecte de pressupost",
+    "data_link": "",
+    "data_title": "Descarrega les dades",
+    "uservoice": "",
+    "base_headline": "PRESSUPOST INGRESSOS",
+    "gross_cost_label": "Despeses",
+    "revenue_label": "Ingressos",
+    "currency_sufix": " €",
+    "data_url": "data/inca/2017_ajuntament_ingressos.json",
+    // "cache_url": "data/inca/cache.json",    
+    "value": {
+        "label": "Pressupost 2017",
+        "type": "accounts",
+        "year": "2017"
+    },
+    "value2": {
+        "label": "Pressupost 2016",
+        "type": "accounts",
+        "year": "2016"
+    }
+}}};
